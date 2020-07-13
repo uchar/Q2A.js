b6ab669ddcaad5dc54364c5128190977a74b0ac0
Omid <omid.es73@gmail.com>
2020-07-13 02:11:51 -0400

    small fixs

--- front_end/src/pages/index.js
+++ front_end/src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
-import Box from '@material-ui/core/Box';
+import { Typography, Box } from '@material-ui/core';
 import { useQuery } from '@apollo/react-hooks';
 import QuestionItem from '../common/components/QuestionItem';
 import Layout from '../common/components/Layout/Layout';
@@ -41,6 +41,10 @@ function MainPage() {
   return (
     <Layout>
       <Box className={classes.paper}>
+        <div style={{flex:"row"}}>
+
+        </div>
+        <Typography style={{ marginTop: 25, fontSize: 25 }}>آخرین سوالات</Typography>
         {questions &&
           questions.map((question) => {
             return <QuestionItem key={question.id} {...question} />;
