ea162e33ce1ea2f7deee638443c84c12ac55841c
Omid <omid.es73@gmail.com>
2020-07-17 03:54:53 -0400

    Fix run problem

--- front_end/src/apolloClient.js
+++ front_end/src/apolloClient.js
@@ -6,6 +6,7 @@ import fetch from 'isomorphic-unfetch';
 export default function createApolloClient(initialState, ctx) {
   // The `ctx` (NextPageContext) will only be present on the server.
   // use it to extract auth headers (ctx.req) or similar.
+  console.log('Graphql url : ', process.env.GRAPHQL_URL);
   return new ApolloClient({
     ssrMode: Boolean(ctx),
     link: new HttpLink({
