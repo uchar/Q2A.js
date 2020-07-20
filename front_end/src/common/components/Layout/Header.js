0ef3a4f78e04904dd004aa84e4578c1367516dbd
hpnarges <hp.narges8@gmail.com>
2020-07-20 00:17:18 -0400

    Add notification dialog

--- front_end/src/common/components/Layout/Header.js
+++ front_end/src/common/components/Layout/Header.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, IconButton, Typography, InputBase, Badge, MenuItem, Menu } from '@material-ui/core';
+import { fade, makeStyles, withStyles } from '@material-ui/core/styles';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  InputBase,
+  Badge,
+  MenuItem,
+  Menu,
+  Box,
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
@@ -10,8 +20,17 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import Link from 'next/link';
 import { useDispatch, useSelector } from 'react-redux';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import { getStrings } from '../../utilities';
-import { wrapper } from '../../../redux/store';
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -57,7 +76,6 @@ const useStyles = makeStyles((theme) => ({
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
     transition: theme.transitions.create('width'),
     width: '100%',
@@ -77,7 +95,20 @@ const useStyles = makeStyles((theme) => ({
       display: 'none',
     },
   },
+  paper: {
+    marginRight: theme.spacing(2),
+  },
+  notificationsMenu: {
+    maxWidth: '360px',
+    backgroundColor: '#ebebeb',
+  },
 }));
+
+function createData(name) {
+  return { name };
+}
+
+const rows = [createData('Answer'), createData('Answer'), createData('election')];
 
 export default function Header({ store }) {
   const classes = useStyles();
@@ -211,6 +242,89 @@ export default function Header({ store }) {
     }
   };
 
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  const [notification, setNotification] = React.useState(null);
+  const [notificationText, setNotificationText] = React.useState([
+    {
+      id: 1,
+      name: 'answer',
+      date: '7/19/2020',
+      question: ' تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست',
+      answer:
+        ' تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست  تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست',
+    },
+    {
+      id: 1,
+      name: 'comment',
+      date: '7/19/2020',
+      question: ' تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست',
+      answer:
+        ' تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست  تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست',
+    },
+    {
+      id: 1,
+      name: 'comment',
+      date: '7/19/2020',
+      question: ' تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست',
+      answer:
+        ' تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست  تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست',
+    },
+    {
+      id: 1,
+      name: 'comment',
+      date: '7/19/2020',
+      question: ' تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست',
+      answer:
+        ' تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست  تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست',
+    },
+    {
+      id: 1,
+      name: 'comment',
+      date: '7/19/2020',
+      question: ' تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست',
+      answer:
+        ' تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست  تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست',
+    },
+    {
+      id: 1,
+      name: 'comment',
+      date: '7/19/2020',
+      question: ' تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست',
+      answer:
+        ' تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست  تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست',
+    },
+    {
+      id: 1,
+      name: 'comment',
+      date: '7/19/2020',
+      question: ' تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست',
+      answer:
+        ' تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست  تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست رشته ای به عددی و برعکس تبدیل لیست',
+    },
+  ]);
+  const handleClick = (event) => {
+    setNotification(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setNotification(null);
+  };
+
   return (
     <div className={classes.grow}>
       <AppBar color="background.default" position="static">
@@ -239,6 +353,52 @@ export default function Header({ store }) {
           </div>
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
+            <IconButton aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+              <Badge badgeContent={17} color="primary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <Menu
+              style={{ maxWidth: '500px' }}
+              id="simple-menu"
+              anchorEl={notification}
+              anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+              transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+              keepMounted
+              open={Boolean(notification)}
+              onClose={handleClose}
+            >
+              {notificationText.map((row) => (
+                <Box
+                  boxShadow={1}
+                  style={{ cursor: 'pointer', padding: '10px', margin: '10px 10px 10px 10px' }}
+                >
+                  <div
+                    style={{
+                      justifyContent: 'space-between',
+                      display: 'flex',
+                    }}
+                  >
+                    <Typography color={'textSecondary'} style={{ fontSize: '12px' }}>
+                      {row.date}
+                    </Typography>
+                    <Typography color={'textSecondary'} style={{ fontSize: '12px' }}>
+                      {row.name}
+                    </Typography>
+                  </div>
+
+                  <Typography color={'textPrimary'} style={{ wordWrap: 'wordBreak', textAlign: 'right' }}>
+                    {row.question}
+                  </Typography>
+                  <Typography
+                    color={'textSecondary'}
+                    style={{ wordWrap: 'wordBreak', fontSize: '12px', textAlign: 'right' }}
+                  >
+                    {row.answer}
+                  </Typography>
+                </Box>
+              ))}
+            </Menu>
             <IconButton aria-label="show 4 new mails" color="inherit" onClick={handleLanguageMenuOpen}>
               <Translate />
             </IconButton>
@@ -251,11 +411,7 @@ export default function Header({ store }) {
                 />
               }
             </IconButton>
-            <IconButton aria-label="show 17 new notifications" color="inherit">
-              <Badge badgeContent={17} color="primary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
+
             <IconButton
               edge="end"
               aria-label="account of current user"
