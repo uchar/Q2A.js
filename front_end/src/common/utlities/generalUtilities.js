6e73af8c40b1c6b87b43159a1ecb586fc1709f92
Omid <omid.es73@gmail.com>
2020-08-02 12:38:37 +0430

    Fix conflicts

--- front_end/src/common/utlities/generalUtilities.js
+++ front_end/src/common/utlities/generalUtilities.js
@@ -10,6 +10,8 @@ export const getFullUrl = (name) => {
   if (name.includes('http')) {
     return name;
   }
+  return `https://5f05e1ddde8c410011025a1b.liara.space/q2a/7khatcode-${name}`;
+};
 
 export const timeAgo = (time, locale = getLanguage()) => {
   const moment = locale === LANGUAGES.ENGLISH ? engMoment : persianMoment;
