f9028e23179e541e366701e63218f5506871d711
uchar <omid.es73@gmail.com>
2021-04-11 07:19:48 +0430

    Add user query tests

--- back_end/src/queries/user.test.js
+++ back_end/src/queries/user.test.js
@@ -1,5 +1,17 @@
-describe('blog mutations api', () => {
-  test('todo', async () => {
-    expect(true).toBe(true);
+import { getUser } from './user';
+import { makeContext } from '../testUtility';
+
+describe('user query api', () => {
+  test('if getUser with an id work', async () => {
+    const user = global.test_user;
+    const result = await getUser(null, { id: user.publicName }, null);
+    expect(user.id).toBe(result.id);
+    expect(user.publicName).toBe(result.publicName);
+  });
+  test('if getUser with context work (self)', async () => {
+    const user = global.test_user;
+    const result = await getUser(null, {}, makeContext());
+    expect(user.id).toBe(result.id);
+    expect(user.publicName).toBe(result.publicName);
   });
 });
