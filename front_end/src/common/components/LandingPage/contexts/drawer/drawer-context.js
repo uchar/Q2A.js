726703c38cb53861e2156db02b3880106985ec31
hpnarges <hpnarges8@gmail.com>
2021-04-15 16:27:53 +0430

    add landing page

---
+++ front_end/src/common/components/LandingPage/contexts/drawer/drawer-context.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+
+const DrawerContext = createContext({});
+export default DrawerContext;
---
+++ front_end/src/pages/landingPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import drawerWrapper from '../common/components/LandingPage/drawerWrapper';
+
+export default function landingPage() {
+  return <drawerWrapper />;
+}
