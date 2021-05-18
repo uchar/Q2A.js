e60f43c2bb495f605f37406957e976259f9b8348
Omid <omid.es73@gmail.com>
2021-05-18 17:36:01 +0430

    Upgrade packages to latest version (#198)
    Co-authored-by: hpnarges <hpnarges8@gmail.com>

--- frontend/src/common/layouts/JssStylesProvider.js
+++ frontend/src/common/layouts/JssStylesProvider.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import { create } from 'jss';
-import { jssPreset, StylesProvider } from '@material-ui/core/styles';
-import rtl from 'jss-rtl';
 import PropTypes from 'prop-types';
 import { isLanguageRtl } from '../utlities/generalUtilities';
 import { getLanguage } from '../utlities/languageUtilities';
 
-const rtlJss = create({
-  plugins: [...jssPreset().plugins, rtl()],
-});
-
 const JssStylesProvider = (props) => {
   const isRTL = isLanguageRtl(getLanguage());
-  if (isRTL)
-    return (
-      <StylesProvider jss={rtlJss}>
-        <div dir="rtl">{props.children}</div>
-      </StylesProvider>
-    );
+  if (isRTL) return <div dir="rtl">{props.children}</div>;
   return <div dir="ltr">{props.children}</div>;
 };
 JssStylesProvider.propTypes = {
