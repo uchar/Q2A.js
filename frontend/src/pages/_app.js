e7dd4fd3962b73418a5493e9aea760786d637f09
hpnarges <hpnarges8@gmail.com>
2021-07-14 12:51:29 +0430

    update packages

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import rtlPlugin from 'stylis-plugin-rtl';
 import { useSelector } from 'react-redux';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -11,10 +12,12 @@ import createCache from '@emotion/cache';
 import { wrapper } from '../redux/store';
 import { darkTheme, lightTheme } from '../common/theme';
 import '../common/globalStyles.css';
-import { getStrings, updateLanguageBaseOnUrl } from '../common/utlities/languageUtilities';
+import { getLanguage, getStrings, updateLanguageBaseOnUrl } from '../common/utlities/languageUtilities';
 import 'nprogress/nprogress.css';
+
 // import * as ga from '../analytics/index';
 import * as gtag from '../libs/gtag';
+import { isLanguageRtl } from '../common/utlities/generalUtilities';
 
 NProgress.configure({ showSpinner: true });
 
@@ -33,19 +36,22 @@ Router.onRouteChangeError = () => {
 export function reportWebVitals(metric) {
   console.log(metric);
 }
-export const cache = createCache({ key: 'css', prepend: true });
+const cache = createCache({ key: 'css' });
+
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [rtlPlugin],
+});
+cache.compat = true;
 
 const Q2aApp = (props) => {
   const { Component, pageProps } = props;
   const themeType = useSelector((state) => state.currentUser.theme);
   const router = useRouter();
   updateLanguageBaseOnUrl(router.locale);
+  const isRTL = isLanguageRtl(getLanguage());
 
   React.useEffect(() => {
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
     const handleRouteChange = (url) => {
       gtag.pageview(url);
     };
@@ -57,17 +63,15 @@ const Q2aApp = (props) => {
 
   const getLayout = Component.getLayout || ((page) => page);
   return (
-    <CacheProvider value={cache}>
-      <React.Fragment>
-        <Head>
-          <title>{getStrings().SITE_TITLE}</title>
-          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        </Head>
-        <ThemeProvider theme={themeType === 'dark' ? darkTheme : lightTheme}>
-          <CssBaseline />
-          {getLayout(<Component {...pageProps} />)}
-        </ThemeProvider>
-      </React.Fragment>
+    <CacheProvider value={isRTL ? cacheRtl : cache}>
+      <Head>
+        <title>{getStrings().SITE_TITLE}</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={themeType === 'dark' ? darkTheme : lightTheme}>
+        <CssBaseline />
+        {getLayout(<Component {...pageProps} />)}
+      </ThemeProvider>
     </CacheProvider>
   );
 };
